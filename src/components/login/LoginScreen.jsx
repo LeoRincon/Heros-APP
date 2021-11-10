bd4e1a86b7df42fe6fee03ac3b8117f231b57d07
Leonardo Rincón <jleobotache@gmail.com>
2021-11-10 07:21:11 -0500

    add login

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginScreen = () => {
+  let history = useNavigate();
+
+  const handleLogin = () => {
+    history('/dc');
+    console.log(history);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='container mt-5'>
+      <h2>Login</h2>
+      <hr />
+      <button className='btn btn-primary' onClick={handleLogin}>
+        login
+      </button>
     </div>
   );
 };
