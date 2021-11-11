f304c842ba5dd995f45f08261722b30d8158bdec
Leonardo Rincón <jleobotache@gmail.com>
2021-11-11 07:34:27 -0500

    add animate a teh components

--- src/components/heroes/HeroList.jsx
+++ src/components/heroes/HeroList.jsx
@@ -7,7 +7,7 @@ export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
-    <div className='row'>
+    <div className='row animate__animated animate__fadeIn'>
       {heroes.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
